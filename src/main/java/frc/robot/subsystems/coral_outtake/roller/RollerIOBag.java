cd1815fb6e1c7a072d9bf18a3a1ee9a9c134d7f1
G3-Robotics <students@g3robotics.com>
2025-01-13 18:00:54 -0500

    Smelled Rohan's smelly beard (and wrote some code [RollerIO Bag Motors Completed])

--- src/main/java/frc/robot/subsystems/coral_outtake/roller/RollerIOBag.java
+++ src/main/java/frc/robot/subsystems/coral_outtake/roller/RollerIOBag.java
@@ -1,15 +1,40 @@
 package frc.robot.subsystems.coral_outtake.roller;
 
+
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.units.Units;
+import frc.robot.utils.Constants;
+
 public class RollerIOBag implements RollerIO {
+
+    private SparkMax rollerMotor;
+
+    /**
+     * Constructor for Bag Roller
+     */
+    public RollerIOBag(int rollerMotorID) {
+        rollerMotor = new SparkMax(rollerMotorID, MotorType.kBrushed);
+
+        SparkMaxConfig rollerMotorConfig = new SparkMaxConfig();
+        rollerMotorConfig.smartCurrentLimit((int) Constants.BAG_CURRENT_LIMIT.in(Units.Amps));
+        rollerMotor.configure(rollerMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
 
     @Override
     public void setVoltage(int voltage) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setVoltage'");
+        rollerMotor.setVoltage(voltage);
     }
 
     @Override
     public void updateInputs(RollerInputs inputs) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateInputs'");
+        inputs.appliedVoltage = Units.Volts.of(rollerMotor.getAppliedOutput() * rollerMotor.getBusVoltage());
+        inputs.velocity = Units.RPM.of(rollerMotor.getEncoder().getVelocity());
+        inputs.temperature = Units.Celsius.of(rollerMotor.getMotorTemperature());
+        inputs.supplyCurrent = Units.Amps.of(rollerMotor.getOutputCurrent());
+        inputs.position = Units.Rotations.of(rollerMotor.getEncoder().getPosition());
+
     }}
