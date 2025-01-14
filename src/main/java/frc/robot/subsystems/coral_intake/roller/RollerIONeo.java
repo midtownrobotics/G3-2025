31cd1ed104b76dc089b31679f296e8eb4ddc80e1
Woodward Theron Spivey II <woodyspivey@gmail.com>
2025-01-13 21:13:41 -0500

    Implement roller IO with neo

--- src/main/java/frc/robot/subsystems/coral_intake/roller/RollerIONeo.java
+++ src/main/java/frc/robot/subsystems/coral_intake/roller/RollerIONeo.java
@@ -1,11 +1,32 @@
 package frc.robot.subsystems.coral_intake.roller;
 
+import frc.robot.utils.Constants;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.units.Units;
+import lombok.Getter;
+
 public class RollerIONeo implements RollerIO {
+ private @Getter SparkMax beltMotor;
+
+  public RollerIONeo(int beltMotorID) {
+    beltMotor = new SparkMax(beltMotorID, MotorType.kBrushless);
+    beltMotor.configure(new SparkMaxConfig().smartCurrentLimit((int) Constants.NEO_CURRENT_LIMIT.in(Units.Amps))
+    .idleMode(IdleMode.kBrake), 
+    
+    ResetMode.kResetSafeParameters, 
+    PersistMode.kNoPersistParameters);
+  }
 
   @Override
   public void setVoltage(int voltage) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'setVoltage'");
+    beltMotor.setVoltage(voltage);
   }
 
   @Override
