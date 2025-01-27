8a213933f14fb78dbf58c657d62ec68849940c3e
Woodward Theron Spivey II <woodyspivey@gmail.com>
2025-01-27 18:14:26 -0500

    code builds

--- src/main/java/frc/lib/team1648/IOProtectionXboxController.java
+++ src/main/java/frc/lib/team1648/IOProtectionXboxController.java
@@ -6,10 +6,12 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 public class IOProtectionXboxController extends CommandXboxController {
   public double currentRumbleValue;
 
+  /** Constructs IO protected xbox controller. */
   public IOProtectionXboxController(int port) {
     super(port);
   }
 
+  /** Sets the rumble. */
   public void setRumble(RumbleType rumbleType, double value) {
     if (currentRumbleValue != value) {
       super.setRumble(rumbleType, value);
