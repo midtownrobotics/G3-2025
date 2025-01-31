6b178c86e2e01f923d8b7ad68a70f941a1d7ec3a
Mysterium422 <mysterium422@gmail.com>
2025-01-30 19:28:27 -0500

    Adds comment in order to build

--- src/main/java/frc/robot/sensors/Photoelectric.java
+++ src/main/java/frc/robot/sensors/Photoelectric.java
@@ -5,9 +5,6 @@ import edu.wpi.first.wpilibj.DigitalInput;
 public class PhotoElectric {
     private DigitalInput sensor;
 
-    /**
-     * Constructs a PhotoElectric Sensor
-     */
     public PhotoElectric(int DIOPort) {
         this.sensor = new DigitalInput(DIOPort);
     }
