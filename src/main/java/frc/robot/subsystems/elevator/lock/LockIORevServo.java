06663711936d8b626b6955b88d06c435e5dfd3b6
G3-Robotics <students@g3robotics.com>
2025-03-12 20:33:39 -0400

    implement locking servo for rev servo

---
+++ src/main/java/frc/robot/subsystems/elevator/lock/LockIORevServo.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems.elevator.lock;
+
+import static edu.wpi.first.units.Units.Degrees;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj.Servo;
+
+public class LockIORevServo {
+    private final Servo servo;
+    private static final Angle lockedPosition = Degrees.of(35), unlockedPosition = Degrees.of(75);
+
+    /** Lock IO constructor for servo. */
+    public LockIORevServo(int id) {
+        servo = new Servo(id);
+        servo.setBoundsMicroseconds(2500, 0, 1500, 0, 500);
+    }
+
+    /** Enable / disble lock. */
+    public void setLockEnabled(boolean value) {
+        double degrees = value ? lockedPosition.in(Degrees) : unlockedPosition.in(Degrees);
+
+        if (degrees < 0) {
+            degrees = 0;
+          } else if (degrees > 270) {
+            degrees = 270;
+        }
+      
+        servo.setAngle((270-degrees)/270);
+    }
+}
