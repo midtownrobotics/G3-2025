2738fc53c9878a7d78418b4a70f1c19bf6f63e47
Gray486 <me@grayjn.com>
2025-04-17 18:24:35 -0500

    Update at 'Thu Apr 17 18:24:34 CDT 2025'

--- src/main/java/frc/robot/subsystems/coral_outtake_pivot/pivot/OuttakePivotIOKraken.java
+++ src/main/java/frc/robot/subsystems/coral_outtake_pivot/pivot/OuttakePivotIOKraken.java
@@ -15,7 +15,6 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.units.measure.Angle;
