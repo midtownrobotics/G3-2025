806b822e6fa110377246e17115443cfb39a33e32
G3-Robotics <students@g3robotics.com>
2025-04-23 20:21:16 -0400

    Update at 'Wed Apr 23 20:21:16 EDT 2025'

--- src/main/java/frc/robot/subsystems/coral_intake/CoralIntake.java
+++ src/main/java/frc/robot/subsystems/coral_intake/CoralIntake.java
@@ -243,7 +243,7 @@ public class CoralIntake extends SubsystemBase {
         break;
       case HANDOFF:
         if (getPosition().lt(Degrees.of(131))) {
-          desiredBeltVoltage = Goal.HANDOFF.getBeltVoltage();
+          desiredBeltVoltage = Goal.PRE_HANDOFF_ADJUST_CORAL.getBeltVoltage();
         } else {
           desiredBeltVoltage = Goal.HANDOFF.getBeltVoltage();
         }
