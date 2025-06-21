56666eddd3b87572b43eddc086c77a1ec1b56690
Woodward Spivey <woodyspivey@gmail.com>
2025-06-21 14:57:41 -0400

    update intake offset

--- src/main/java/frc/robot/subsystems/coral_intake/CoralIntakeConstants.java
+++ src/main/java/frc/robot/subsystems/coral_intake/CoralIntakeConstants.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems.coral_intake;
 
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Milliseconds;
-import static edu.wpi.first.units.Units.Radians;
 
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Time;
@@ -14,7 +13,7 @@ public class CoralIntakeConstants {
     public static final Angle coralIntakeMaxAngle = Degrees.of(135);
     public static final Angle coralIntakeMinAngle = Degrees.of(-14);
 
-    public static final Angle absoluteEncoderOffset = Radians.of(4.27);
+    public static final Angle absoluteEncoderOffset = Degrees.of(200);
 
     public static final Angle upperSensorTriggeredAngle = Degrees.of(135);
     public static final Angle lowerSensorTriggeredAngle = Degrees.of(-14);
